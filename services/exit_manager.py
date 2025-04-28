3e49e692747c47b0d012c26f89a1e78b45daad21
AtQoom <eyes@naver.com>
2025-04-28 19:37:20 +0900

    Update exit_manager.py

--- services/exit_manager.py
+++ services/exit_manager.py
@@ -1,18 +1,26 @@
-# services/exit_manager.py
-async def check_final_exit(self, price, rsi):
-    if self.position_manager.is_long_position():
-        if rsi < RSI_LONG_ENTRY:  # 롱포지션인데 숏 신호
-            await self.exit_manager.exit_long_all()
-    elif self.position_manager.is_short_position():
-        if rsi > RSI_SHORT_ENTRY:  # 숏포지션인데 롱 신호
-            await self.exit_manager.exit_short_all()
+from core.trader import place_order
+from config.constants import LEVERAGE
 
-async def exit_long_all(self):
-    # 남은 모든 롱 포지션 청산
-    place_order("sell", self.position_manager.position_size, LEVERAGE)
-    self.position_manager.reset_position()
+class ExitManager:
+    def __init__(self, position_manager, strategy):
+        self.position_manager = position_manager
+        self.strategy = strategy
 
-async def exit_short_all(self):
-    # 남은 모든 숏 포지션 청산
-    place_order("buy", self.position_manager.position_size, LEVERAGE)
-    self.position_manager.reset_position()
+    async def check_exit(self, price, rsi):
+        # 기존 1차, 2차 청산 로직
+
+    async def check_final_exit(self, price, rsi):
+        if self.position_manager.is_long_position():
+            if self.strategy.is_opposite_signal(rsi, True):  # True = 롱
+                await self.exit_long_all()
+        elif self.position_manager.is_short_position():
+            if self.strategy.is_opposite_signal(rsi, False):  # False = 숏
+                await self.exit_short_all()
+
+    async def exit_long_all(self):
+        place_order("sell", self.position_manager.position_size, LEVERAGE)
+        self.position_manager.reset_position()
+
+    async def exit_short_all(self):
+        place_order("buy", self.position_manager.position_size, LEVERAGE)
+        self.position_manager.reset_position()
