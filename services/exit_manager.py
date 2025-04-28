0f8e8a635554f1c66b91a347ff332826be49a362
AtQoom <eyes@naver.com>
2025-04-28 18:17:49 +0900

    Update exit_manager.py

--- services/exit_manager.py
+++ services/exit_manager.py
@@ -1,1 +1,10 @@
-# Exit management services
+# services/exit_manager.py
+
+from core.trader import place_order
+from config.constants import LEVERAGE
+
+def exit_long(quantity):
+    place_order("sell", quantity, LEVERAGE)
+
+def exit_short(quantity):
+    place_order("buy", quantity, LEVERAGE)
