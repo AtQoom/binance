aef95cc58a9606fc90d604b2eb724051bd99592f
AtQoom <eyes@naver.com>
2025-04-28 18:21:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# Main server run file
+# main.py
+
+from fastapi import FastAPI
+from services.ticker_listener import listen_ticker
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "GateIO Trading Server Running!"}
+
+@app.on_event("startup")
+async def startup_event():
+    listen_ticker()
