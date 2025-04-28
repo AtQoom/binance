2b3d6079b9d0912bcaa92526759e580e7545ee4a
AtQoom <eyes@naver.com>
2025-04-28 19:23:54 +0900

    Update position_manager.py

--- core/position_manager.py
+++ core/position_manager.py
@@ -1,8 +1,22 @@
-current_position = None  # "long", "short", None
+# core/position_manager.py
 
-def set_position(position):
-    global current_position
-    current_position = position
+class PositionManager:
+    def __init__(self):
+        self.position_size = 0
+        self.avg_entry_price = 0
+        self.entry_count = 0
 
-def get_position():
-    return current_position
+    def update_position(self, entry_price, quantity):
+        total_cost = self.avg_entry_price * self.position_size
+        new_cost = entry_price * quantity
+        self.position_size += quantity
+        self.avg_entry_price = (total_cost + new_cost) / self.position_size
+        self.entry_count += 1
+
+    def reset_position(self):
+        self.position_size = 0
+        self.avg_entry_price = 0
+        self.entry_count = 0
+
+    def is_in_position(self):
+        return self.position_size > 0
