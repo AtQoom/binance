0505c7425c3b31bf09ec61164d43d3fb88a24f3a
AtQoom <eyes@naver.com>
2025-04-28 19:23:40 +0900

    Update heikin_ashi.py

--- core/heikin_ashi.py
+++ core/heikin_ashi.py
@@ -1,3 +1,18 @@
-def calculate_heikin_ashi(open_price, high_price, low_price, close_price):
-    ha_close = (open_price + high_price + low_price + close_price) / 4
-    return ha_close
+# core/heikin_ashi.py
+
+import pandas as pd
+
+def calculate_heikin_ashi(df):
+    ha_df = df.copy()
+
+    ha_df['HA_Close'] = (df['open'] + df['high'] + df['low'] + df['close']) / 4
+    ha_open = [(df['open'][0] + df['close'][0]) / 2]
+
+    for i in range(1, len(df)):
+        ha_open.append((ha_open[i-1] + ha_df['HA_Close'][i-1]) / 2)
+
+    ha_df['HA_Open'] = ha_open
+    ha_df['HA_High'] = ha_df[['HA_Open', 'HA_Close', 'high']].max(axis=1)
+    ha_df['HA_Low'] = ha_df[['HA_Open', 'HA_Close', 'low']].min(axis=1)
+
+    return ha_df[['HA_Open', 'HA_High', 'HA_Low', 'HA_Close']]
