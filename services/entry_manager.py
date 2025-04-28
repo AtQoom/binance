b29ee2259d71300a63a40518db678aca9c0ccafc
AtQoom <eyes@naver.com>
2025-04-28 18:50:41 +0900

    Update entry_manager.py

--- services/entry_manager.py
+++ services/entry_manager.py
@@ -1,10 +1,10 @@
-# Entry management services# services/entry_manager.py
+from config.constants import *
+from core.position_manager import set_position
 
-from core.trader import place_order
-from config.constants import LEVERAGE
+def enter_position(direction, account_balance, price, strong=False):
+    percent = ENTRY_PERCENT_STRONG if strong else ENTRY_PERCENT_NORMAL
+    usd_amount = account_balance * percent
+    qty = usd_amount / price
 
-def enter_long(quantity):
-    place_order("buy", quantity, LEVERAGE)
-
-def enter_short(quantity):
-    place_order("sell", quantity, LEVERAGE)
+    set_position(direction)
+    return qty
