adde984253160f50fd37cc21920d85c83dd54502
AtQoom <eyes@naver.com>
2025-04-29 09:19:31 +0900

    Update position_manager.py

--- core/position_manager.py
+++ core/position_manager.py
@@ -5,21 +5,23 @@ class PositionManager:
         self.position_size = 0
         self.avg_entry_price = 0
         self.entry_count = 0
-        self.is_long_position = None
 
-    def update_position(self, entry_price, quantity, is_long):
+    def update_position(self, entry_price, quantity):
         total_cost = self.avg_entry_price * self.position_size
         new_cost = entry_price * quantity
         self.position_size += quantity
         self.avg_entry_price = (total_cost + new_cost) / self.position_size
         self.entry_count += 1
-        self.is_long_position = is_long
+        print(f"[PositionManager] Updated Position: size={self.position_size}, avg_entry_price={self.avg_entry_price}")
 
     def reset_position(self):
+        print("[PositionManager] Resetting position.")
         self.position_size = 0
         self.avg_entry_price = 0
         self.entry_count = 0
-        self.is_long_position = None
 
     def is_in_position(self):
+        return self.position_size != 0
+
+    def is_long(self):
         return self.position_size > 0
