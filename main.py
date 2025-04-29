df011453ee8dac53b36ce1613b8e8a1b2937591a
AtQoom <eyes@naver.com>
2025-04-29 10:20:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # main.py
 from fastapi import FastAPI
-import asyncio
 from services.ticker_listener import listen_ticker
+import asyncio
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+async def root():
     return {"message": "GateIO Trading Server Running!"}
 
 @app.on_event("startup")
-async def startup_event():
-    loop = asyncio.get_event_loop()
-    loop.create_task(listen_ticker())  # 명시적으로 비동기 루프에 task 등록
+async def app_startup():
+    print("Starting ticker listener...")
+    asyncio.create_task(listen_ticker())
